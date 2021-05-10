a8a92e13e93cfb3945a2d0aaffa2b46f10082e6f
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2021-05-10 20:34:55 +0200

    1️⃣  V.1.0 🎉 First commit : Adapting Rimbo flow to Big Demo 🎉.

---
+++ client/src/screens/RegisterTenant_RJ2/tenant-reducer.js
@@ -0,0 +1,23 @@
+import { UPDATE_NEWTENANT_INFO } from "./tenant-constants";
+
+export const DefaultTenant = {
+  monthlyNetIncome: "",
+  jobType: "",
+  documentType: "",
+  documentNumber: "",
+  isAcceptedPrivacy: true,
+  stageOne: 5,
+};
+
+export const TenantReducer = (newTenant, { type, payload }) => {
+  switch (type) {
+    case UPDATE_NEWTENANT_INFO:
+      return {
+        ...newTenant,
+        ...payload,
+      };
+
+    default:
+      return newTenant;
+  }
+};
